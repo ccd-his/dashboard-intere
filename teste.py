4546f879f35ce88124bcae62af66dba8c46dce7d
Marina <ma.monteiro.m@gmail.com>
2026-07-19 11:36:01 -0300

    logo e páginas acertadas

--- teste.py
+++ teste.py
@@ -5,9 +5,6 @@ import pandas as pd
 
 from flask import render_template
 
-df = pd.read_csv(
-    "https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv"
-)
 
 
 class CustomDash(Dash):
@@ -47,17 +44,9 @@ app.title = "Índice de Resiliência Climática Territorial"
 
 
 app.layout = [
-    html.H1(children="Title of Dash App", style={"textAlign": "center"}),
-    dcc.Dropdown(df.country.unique(), "Canada", id="dropdown-selection"),
-    dcc.Graph(id="graph-content"),
     dash.page_container
 ]
 
-
-@callback(Output("graph-content", "figure"), Input("dropdown-selection", "value"))
-def update_graph(value):
-    dff = df[df.country == value]
-    return px.line(dff, x="year", y="pop")
 
 
 if __name__ == "__main__":
